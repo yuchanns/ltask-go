98ba39bcb87e875676c8982199580bf5a592967e
Hanchin Hsieh <me@yuchanns.xyz>
2025-07-27 16:40:15 +0800

    fix(timer): add annotation

--- ltask.go
+++ ltask.go
@@ -86,6 +86,8 @@ func (task *ltask) init(L *lua.State, config *ltaskConfig) {
 	}
 	refEvent = &event
 	task.event = &event
+	// Windows compatiblity: initialize the timer with a nil value
+	// to clear any wired data in the memory.
 	task.timer = nil
 }
 
