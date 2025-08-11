71a20a3f47d5a80ab467ae43991f62d86c852557
Hanchin Hsieh <me@yuchanns.xyz>
2025-08-11 11:51:15 +0800

    test: add request

--- test/bootstrap.lua
+++ test/bootstrap.lua
@@ -8,6 +8,25 @@ local addr = ltask.spawn("user", "Hello")
 
 print("Spawn user", addr)
 
+--- test request
+
+local tasks = {
+  { ltask.call, addr, "wait", 30, id = 1 },
+  { ltask.call, addr, "wait", 20, id = 2 },
+  { ltask.call, addr, "wait", 10, id = 3 },
+  { ltask.call, addr, "wait", 5, id = 4 },
+  { ltask.sleep, 25 },
+}
+
+for req, resp in ltask.parallel(tasks) do
+  if not req.id then break end
+  if not resp.error then
+    print("REQ", req.id, resp[1])
+  else
+    print("ERR", req.id, resp.error)
+  end
+end
+
 ltask.send(addr, "exit")
 
 print("Bootstrap End")
--- test/user.lua
+++ test/user.lua
@@ -7,6 +7,35 @@ local worker = ltask.worker_id()
 print(string.format("User %d in worker %d", ltask.self(), worker))
 ltask.worker_bind(worker) -- bind to current worker thread
 
+local function coroutine_test()
+  coroutine.yield("Coroutine yield")
+  return "Coroutine end"
+end
+
+-- FIXME: Why the creation of coroutine raises root init_receipt error?
+-- local co = coroutine.create(coroutine_test)
+-- while true do
+--   local ok, ret = coroutine.resume(co)
+--   if ok then
+--     print(ret)
+--   else
+--     break
+--   end
+-- end
+
+function S.wait(ti)
+  if ti < 10 then error("Error : " .. ti) end
+  ltask.sleep(ti)
+  return ti
+end
+
+function S.req(ti)
+  print("Wait Req", ti)
+  ltask.sleep(ti)
+  print("wait resp", ti)
+  return ti
+end
+
 function S.exit() ltask.quit() end
 
 return S
