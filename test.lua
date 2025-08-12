a7a2c34958b847f049632013c8a958c50eb94bc8
Hanchin Hsieh <me@yuchanns.xyz>
2025-08-12 11:29:33 +0800

    fix(test): avoid stuck of tests in ci

--- test.lua
+++ test.lua
@@ -3,6 +3,7 @@ local start = require("test.start")
 start({
   core = {
     -- debuglog = "=", -- stdout
+    worker = 3, -- 2 free workers + 1 binding worker for service user
   },
   service_path = "service/?.lua;test/?.lua",
   bootstrap = {
