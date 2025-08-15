beccbe30b8c11ba07b95b34baf0aa35230d6476c
Hanchin Hsieh <me@yuchanns.xyz>
2025-08-15 12:59:20 +0800

    ci: add bee to luamake

--- examples/webserver/make.lua
+++ examples/webserver/make.lua
@@ -1,13 +1,13 @@
 local lm = require("luamake")
 
 local bindir = "build/bin"
+lm.bindir = bindir
 
 lm:dll("lua54")({
   sources = { "lua54/*.c", "!lua54/onelua.c", "!lua54/lua.c", "!lua54/luac.c", "!lua54/ltests.c" },
   includes = {
     "lua54",
   },
-  bindir = bindir,
 
   visibility = "default",
   links = {
@@ -49,14 +49,26 @@ local output = bindir
   .. "/"
   .. (lm.os == "windows" and "lua54.dll" or (lm.os == "macos" and "liblua54.dylib" or "liblua54.so"))
 
-if lm.os == "windows" then
-  lm:default({ "lua54" })
-else
-  lm:copy("copy")({
-    deps = { "lua54" },
-    inputs = { bindir .. "/lua54.so" },
-    outputs = { output },
-  })
+lm:copy("copy54")({
+  deps = { "lua54" },
+  inputs = { bindir .. "/lua54.so" },
+  outputs = { output },
+})
 
-  lm:default({ "lua54", "copy" })
-end
+lm.EXE = "lua"
+lm:import("bee/make.lua")
+
+local bee_output = bindir
+  .. "/"
+  .. (lm.os == "windows" and "bee.dll" or (lm.os == "macos" and "libbee.dylib" or "libbee.so"))
+lm:copy("copy_bee")({
+  deps = { "lua54" },
+  inputs = { bindir .. "/bee.so" },
+  outputs = { bee_output },
+})
+
+lm:phony("all")({
+  deps = { "lua54", lm.os ~= "windows" and "copy54", "bee", lm.os ~= "windows" and "copy_bee" },
+})
+
+lm:default({ "all" })
